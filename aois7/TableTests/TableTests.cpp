2274db7c3bac2b3910a1c2fd17d146028604240e
AlexeyKharietskiy <113918037+AlexeyKharietskiy@users.noreply.github.com>
2024-06-01 12:53:39 +0300

    Update TableTests.cpp

--- aois7/TableTests/TableTests.cpp
+++ aois7/TableTests/TableTests.cpp
@@ -1,4 +1,4 @@
-﻿#include "pch.h"
+#include "pch.h"
 #include "CppUnitTest.h"
 #include"..//aois7/Table.cpp"
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
@@ -33,7 +33,7 @@ namespace TableTests
 		{
 			vector<vector<bool>> table1 = {
 	   {0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0},
-	   {1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0},
+	   {1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0},	
 	   {0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0},
 	   {1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1},
 	   {1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0},
@@ -50,10 +50,15 @@ namespace TableTests
 	   {0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0}
 			};
 			Table tb1(table1);
+			vector<bool> row;
 			tb1.do_function_4(0, 1, 5);
+			Assert::AreEqual(vector<bool>({ 0,0,0,0,0,0,0,0,1,0,1,1,0,0,0,1 }) , tb1.get_word(5));
 			tb1.do_function_6(2, 3, 7);
+			Assert::AreEqual(vector<bool>({ 1,1,1,1,1,0,1,1,1,0,0,1,0,0,0,0 }), tb1.get_word(7));
 			tb1.do_function_9(3, 12, 15);
+			Assert::AreEqual(vector<bool>({ 1,1,0,1,0,1,1,0,1,1,1,1,0,1,0,1 }), tb1.get_word(15));
 			tb1.do_function_11(5, 8, 11);
+			Assert::AreEqual(vector<bool>({ 0,1,1,0,1,1,0,0,1,0,1,1,0,1,0,1 }), tb1.get_word(11));
 			tb1.show_table();
 			Assert::AreEqual(vector<bool>({0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0}), tb1.get_address_column(7));
 		}
@@ -80,6 +85,7 @@ namespace TableTests
 			};
 			Table tb2(table2);
 			tb2.sum_words({ 1, 0, 0 });
+			Assert::AreEqual(vector<bool>({ 1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,0 }), tb2.get_word(1));
 			tb2.update_address_column({ 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0 }, 4);
 			tb2.show_vec(tb2.get_address_column(4));
 			tb2.show_vec(tb2.get_word(1));
